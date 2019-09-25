5a902b6692c72625e74adb6e5453a9a5635751ac
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-09-25 09:07:43 +0300

    add busboy

---
+++ api/form.js
@@ -0,0 +1,45 @@
+const inspect = require(`util`).inspect;
+
+const Busboy = require(`busboy`);
+
+module.exports = (req, res) => {
+  if (req.method === `POST`) {
+    const busboy = new Busboy({headers: req.headers});
+    const data = {};
+    busboy.on(`file`, function (fieldname, file, filename, encoding, mimetype) {
+      console.log(`File [${fieldname}]: filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`);
+      let size = 0;
+      file.on(`data`, function (data) {
+        size += data.length;
+        console.log(`File [${fieldname}] got ${data.length} bytes`);
+      });
+      file.on(`end`, function () {
+        console.log(`File [${fieldname}] Finished`);
+        data[fieldname] = `File: ${filename}. Size: ${size} bytes`;
+      });
+    });
+    busboy.on(`field`, function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      console.log(`Field [${fieldname}](${fieldnameTruncated}: value: ${inspect(val)} (${valTruncated}), encoding: ${encoding}, mimetype: ${mimetype}`);
+      data[fieldname] = val;
+    });
+    busboy.on(`finish`, function () {
+      console.log(`Done parsing form!`);
+      res.json(data);
+      res.end();
+    });
+    req.pipe(busboy);
+  } else if (req.method === `GET`) {
+    res.writeHead(200, {Connection: `close`});
+    res.end(`<!doctype html>
+                <html lang="en">
+                <head><title>Test Form Post</title></head>
+                <body>
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        <input type="text" name="textfield"><br />
+                        <input type="file" name="filefield"><br />
+                        <input type="submit">\
+                    </form>
+                </body>
+                </html>`);
+  }
+};
