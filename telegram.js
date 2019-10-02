5870f258519ebeffed6cb82a3429da7529735c1a
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-10-02 16:37:11 +0300

    add some logging and move init

--- telegram.js
+++ telegram.js
@@ -4,24 +4,29 @@ const TelegramBot = require(`node-telegram-bot-api`);
 const token = process.env.BOT_TOKEN;
 const rootId = process.env.ROOT_ID;
 
-// Create a bot that uses `polling` to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on(`message`, (msg) => {
-  const me = msg.from;
-  let message;
-  if (!me) {
-    message = `You are anonymous =(`;
-  } else {
-    message = `Here what I know about you:
-id:${me.id} ${me.first_name || ``} ${me.last_name || ``}${me.username ? ` @${me.username}` : ``}`;
-  }
-  bot.sendMessage(msg.chat.id, message, {parse_mode: `Markdown`});
-});
+// // Create a bot that uses `polling` to fetch new updates
+// const bot = new TelegramBot(token, {polling: true});
+//
+// // Listen for any kind of message. There are different kinds of
+// // messages.
+// bot.on(`message`, (msg) => {
+//   const me = msg.from;
+//   let message;
+//   if (!me) {
+//     message = `You are anonymous =(`;
+//   } else {
+//     message = `Here what I know about you:
+// id:${me.id} ${me.first_name || ``} ${me.last_name || ``}${me.username ? ` @${me.username}` : ``}`;
+//   }
+//   bot.sendMessage(msg.chat.id, message, {parse_mode: `Markdown`});
+// });
 
 const sendToRoot = (message) => {
+  console.log(`Token length: ${token.length}`);
+  console.log(`Root id: ${rootId}`);
+  // Create a bot that uses `polling` to fetch new updates
+  const bot = new TelegramBot(token, {polling: true});
+
   console.log(`Sending message: ${message}`);
   bot.sendMessage(rootId, message);
 };
