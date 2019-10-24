56aba4441d7af2cb1b83bf646fe9a36d8ebfadc4
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-10-24 20:49:22 +0300

    update bash script

--- curl.sh
+++ curl.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-data='{"chat_id":"'$2'","text": "Hello World!"}'
-echo $data
+data=('{"chat_id":"'"$2"'","text": "Hello World!"}')
+json="${data[*]}"
+echo "${json}"
 
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data $data \
+  --data $json \
   -v \
   https://simple-http-proxy.zeckson.now.sh/bot$1/sendMessage
