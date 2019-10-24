5a1674ff2b91b5127cc5c494dbed67ce75faa368
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-10-24 21:22:20 +0300

    fix minor issues add redirect_uri

--- api/form.js
+++ api/form.js
@@ -2,33 +2,45 @@ const send = require(`../src/urlsender`);
 const loadForm = require(`../src/formloader`);
 const formatter = require(`../src/dataformatter.js`);
 
-const TEST_PAGE = `
+const GET_RESPONSE = `
 <!doctype html>
 <html lang="en">
-<head><title>Test Form Post</title></head>
+<head><title>Not allowed</title></head>
 <body>
-  <form action="" method="POST" enctype="multipart/form-data" accept-charset="utf-8">
-    <input type="text" name="textfield"><br />
-    <input type="file" name="filefield"><br />
-    <input type="submit">\
-  </form>
+  <h2>Method is not allowed</h2>
+</body>
+</html>`;
+
+const SUCCESS_SEND_RESPONSE = `
+<!doctype html>
+<html lang="en">
+<head><title>Form successfully sent!</title></head>
+<body>
+  <h2>Data has been sent!</h2>
 </body>
 </html>`;
 
 module.exports = (req, res) => {
   switch (req.method) {
     case `GET`:
-      res.writeHead(200, {Connection: `close`});
-      res.end(TEST_PAGE);
+      res.writeHead(405, {Connection: `close`});
+      res.end(GET_RESPONSE);
       break;
     case `POST`:
       loadForm(req, (data) => {
+        const redirectUri = data['redirect_uri'];
+        delete data['redirect_uri'];
+
         const formatted = formatter.toUserFriendly(data);
-        console.log(formatted);
         const sent = send(formatted);
-        console.log(sent);
-        sent.then((data) => {
-          res.send(data);
+        sent.then(() => {
+          if (redirectUri) {
+            res.writeHead(302, {
+              'location': redirectUri
+            });
+          } else {
+            res.send(SUCCESS_SEND_RESPONSE);
+          }
           res.end();
         }).catch((err) => {
           res.status(503).send(err);
@@ -37,6 +49,6 @@ module.exports = (req, res) => {
       });
       break;
     default:
-      // Do nothing by default
+    // Do nothing by default
   }
 };
